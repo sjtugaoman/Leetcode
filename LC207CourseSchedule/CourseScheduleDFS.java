ccdfe912962b5d8c5baa21d8818e560907bb279a
Man Gao <ManGao@koumandeMacBook-Pro.local>
2018-03-27 14:19:54 -0400

    LC210

--- LC207CourseSchedule/CourseScheduleDFS.java
+++ LC207CourseSchedule/CourseScheduleDFS.java
@@ -1,4 +1,47 @@
 //DFS solution using backtracking
-class CourseScheduleDFS {
+import java.util.*;
 
+class CourseSchedule {
+    // use a separate hashmap to record the DFS pruning
+    public static HashMap<Integer, Boolean> memo = new HashMap<Integer, Boolean>();
+
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
+        //construct hashmap of neighbours
+        HashMap<Integer, HashSet<Integer>> neighbors = new HashMap<Integer, HashSet<Integer>>();
+        HashSet<Integer> curPath = new HashSet<Integer>();
+        for(int i = 0; i < prerequisites.length; i++) {
+          if(!neighbors.containsKey(prerequisites[i][1])) {
+            neighbors.put(prerequisites[i][1], new HashSet<Integer>());
+          }
+          neighbors.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        //Find if there is any cycle using DFS
+        for(int[] pair: prerequisites) {
+          if(DFSHelper(neighbors, curPath, pair[0], -1)) return false;
+        }
+        return true;
+    }
+
+
+    public static boolean DFSHelper(HashMap<Integer, HashSet<Integer>> neighbors, HashSet<Integer> curPath, int kid, int parent) {
+      //using DFS to check if there is cycle
+      if(memo.containsKey(kid)) return memo.get(kid);
+      if(curPath.contains(kid)) return true;
+      if(!neighbors.containsKey(kid)) return false;
+
+      curPath.add(kid);
+      for(int neighbor: neighbors.get(kid)){
+        boolean hasCycle = DFSHelper(neighbors, curPath, neighbor, kid);
+        memo.put(kid, hasCycle);
+        if(hasCycle) return true;
+      }
+      curPath.remove(kid);
+      return false;
+    }
+
+    public static void main(String[] args) {
+      int numCourses = 4;
+      int[][] prerequisites = {{1,0}, {2,0}, {3,1}, {3,2}};
+      System.out.println(canFinish(numCourses, prerequisites));
+    }
 }
--- LC210CourseScheduleII/CourseScheduleII.java
+++ LC210CourseScheduleII/CourseScheduleII.java
@@ -1,5 +1,47 @@
+import java.util.*;
+
 class CourseScheduleII {
-  public int[] findOrder(int numCourses, int[][]prerequisites) {
-  
-  }
+  //BFS
+    public static int[] findOrder(int numCourses, int[][]prerequisites) {
+
+        //corner case
+        if(numCourses == 0) return new int[0];
+        //construct the indegree[]
+        int[] indegree = new int[numCourses];
+        int[] order = new int[numCourses];
+        int index = 0;
+        for(int i = 0; i < prerequisites.length; i++) {
+          indegree[prerequisites[i][0]]++;
+        }
+        //build queue for course do not have indegree
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for(int i = 0; i < numCourses; i++) {
+          if(indegree[i] == 0) {
+            queue.offer(i);
+            order[index++] = i;
+          }
+        }
+
+        while(!queue.isEmpty()){
+          int course = queue.poll();
+          for(int i = 0; i < prerequisites.length; i++) {
+            if(prerequisites[i][1] == course) {
+              indegree[prerequisites[i][0]]--;
+              if(indegree[prerequisites[i][0]] == 0) {
+                queue.offer(prerequisites[i][0]);
+                order[index++] = prerequisites[i][0];
+              }
+            }
+          }
+        }
+
+        return index == numCourses ? order : new int[0];
+    }
+
+    public static void main(String[] args) {
+      int numCourses = 4;
+      int[][] prerequisites = {{1,0}, {2,0}, {3,1}, {3,2}};
+      int[] result = findOrder(numCourses, prerequisites);
+      System.out.println(Arrays.toString(result));
+    }
 }
